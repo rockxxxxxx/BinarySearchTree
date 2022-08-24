7a40cafffa9abe9f523834e3765f905e65c32c25
Rakesh Raushan <raushanrakesh069@gmail.com>
2022-08-24 23:13:56 +0530

    Pre order traversal using recursion

---
+++ recursivePreOderTraversal.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+}
